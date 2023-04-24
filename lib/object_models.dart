b52d590c8df2c2570abd8345555215ce8ce6730e
Richiity <39109271+Richiity@users.noreply.github.com>
2023-04-24 12:41:42 -0700

    init push. boilerplate code

---
+++ lib/object_models.dart
@@ -0,0 +1,38 @@
+class Trip {
+  DateTime start;
+  DateTime end;
+  String destination;
+  Flight flight;
+  Hotel hotel;
+  int budget;
+  Trip({
+    required this.start,
+    required this.end,
+    required this.destination,
+    required this.flight,
+    required this.hotel,
+    required this.budget
+  });
+}
+class Flight {
+  String departure;
+  String arrival;
+  String flightNum;
+  Flight({
+    required this.departure,
+    required this.arrival,
+    required this.flightNum
+  });
+}
+
+class Hotel{
+  DateTime checkIn;
+  DateTime checkOut;
+  int roomNum;
+
+  Hotel({
+    required this.checkIn,
+    required this.checkOut,
+    required this.roomNum
+  });
+}
