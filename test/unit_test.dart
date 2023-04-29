d6c04b687a3d453792a1b6c4a4c8a7ac43f4c419
Casey Nguyen <caseyjnguyen@gmail.com>
2023-04-29 14:31:41 -0700

    Unit test outlining.

---
+++ test/unit_test.dart
@@ -0,0 +1,37 @@
+// unit_test.dart
+// Casey Nguyen
+// PURPOSE: This file runs unit tests for our TravelApp.
+//
+// HISTORY
+// 4.29.2023: Tests outlined based on current master.
+
+// Imports
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:travel_app/object_models.dart';
+
+/*
+class Trip {
+  DateTime start;
+  DateTime end;
+  String destination;
+  Flight flight;
+  Hotel hotel;
+  int budget;
+  Trip({
+    required this.start,
+    required this.end,
+    required this.destination,
+    required this.flight,
+    required this.hotel,
+    required this.budget
+  });
+}
+ */
+
+// Main
+main(){
+  group("Trip Logic:", (){
+    test("Reject adding trip if start time exceeds end time", (){
+    });
+  });
+}
