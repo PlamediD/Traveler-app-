0bdfb85b73ae2884db233a0930f382540c116ba1
Casey Nguyen <caseyjnguyen@gmail.com>
2023-04-30 22:07:28 -0700

    New tests.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,55 +6,37 @@
 //
 // HISTORY
 // 4.29.2023: Tests outlined based on current master.
+// 4.30.2023: Testing runs.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+import 'package:travel_app/form_buttons.dart';
 import 'package:travel_app/forms.dart';
+import 'package:travel_app/main.dart';
 
 main(){
-  group("Incorrect time submission logic", (){
-    testWidgets('If no dates are added, an error message appears',
-            (WidgetTester tester) async {
-          await tester.pumpWidget(
-              const MaterialApp(
-                  home: Scaffold(
-                      body: Forms()
-                  )
-              )
-          );
-          final submit = find.text("Submit");
-          await tester.tap(submit);
-          await tester.pump();
-          expect(find.text('Please select a start date and time'), findsOneWidget);
-    });
-    testWidgets('If start time exceeds end time, shoot an error.',
-            (WidgetTester tester) async {
-          await tester.pumpWidget(
-              const MaterialApp(
-                  home: Scaffold(
-                      body: Forms()
-                  )
-              )
-          );
-          // Find our buttons
-          final startButton = find.text("Start Time");
-          final endButton = find.text("End Time");
-          final submit = find.text("Submit");
+  // EXPECT: Main menu runs.
+   testWidgets("Check that the main menu runs",
+   (WidgetTester tester) async{
+     await tester.pumpWidget(
+       const MaterialApp(
+         home: Scaffold(
+           body: MyApp()
+         )
+       )
+     );
+     expect(find.byIcon(Icons.add), findsOneWidget);
+   });
 
-          // Go through a datetime pick flow.
-          await tester.tap(startButton);
-          await tester.pump();
-          final arrow = find.byIcon(Icons.arrow_forward_ios);
-          await tester.tap(arrow);
-          await tester.pump();
-          final startPick = find.text("2");
-          await tester.tap(startPick);
-          await tester.pump();
-          final ok = find.text("OK");
-
-          await tester.tap(submit);
-          await tester.pump();
-          expect(find.text('Please select a start date and time'), findsOneWidget);
-        });
-  });
+   testWidgets("Check the forms page opens",
+           (WidgetTester tester) async{
+         await tester.pumpWidget(
+             const MaterialApp(
+                 home: Scaffold(
+                     body: MyApp()
+                 )
+             )
+         );
+       });
 }
