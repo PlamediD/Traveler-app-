f9f4c365eed097aca3ea4eb824dde44345ec638c
Plamedi Diakubama <87151146+PlamediD@users.noreply.github.com>
2023-05-18 00:52:49 -0700

    Update object_models.dart
    Attempt to implement budget tracker. I put them here because Henry is working on weather and I do not want to mess up things

--- lib/object_models.dart
+++ lib/object_models.dart
@@ -1,31 +1,69 @@
-class Trip {
+import 'package:flutter/cupertino.dart';
+
+class Trip with ChangeNotifier{
   DateTime start;
   DateTime end;
   String destination;
   Flight flight;
   Hotel hotel;
   int budget;
+  List<Expense> ?expenses;
+
   Trip({
     required this.start,
     required this.end,
     required this.destination,
     required this.flight,
     required this.hotel,
-    required this.budget
+    required this.budget,
+    this.expenses ,
   });
+
+  void addExpense(String category, int amount) {
+    final expense = Expense(category: category, amount: amount);
+    expenses ??= []; // Initialize the list if it's null
+    expenses!.add(expense);
+    notifyListeners();
+
+  }
+
+
+  int getTotalExpenses() {
+    if (expenses == null) {
+      return 0;
+    }
+
+    return expenses!.fold(0, (sum, expense) => sum + expense.amount);
+  }
+
+  int getRemainingBudget() {
+
+    return budget - getTotalExpenses();
+
+  }
+
 }
+
+class Expense {
+  String category;
+  int amount;
+
+  Expense({required this.category, required this.amount});
+}
+
 class Flight {
   String departure;
   String arrival;
   String flightNum;
+
   Flight({
     required this.departure,
     required this.arrival,
-    required this.flightNum
+    required this.flightNum,
   });
 }
 
-class Hotel{
+class Hotel {
   DateTime checkIn;
   DateTime checkOut;
   int roomNum;
@@ -33,6 +71,6 @@ class Hotel{
   Hotel({
     required this.checkIn,
     required this.checkOut,
-    required this.roomNum
+    required this.roomNum,
   });
 }
