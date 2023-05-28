ef74e8cc7f9f5c7b525d540029117f15f69d739c
Plamedi Diakubama <87151146+PlamediD@users.noreply.github.com>
2023-05-27 21:33:48 -0700

    Update trip_list.dart
    1. Switch to using firebase as database
    2. Implemented the packing list feature

--- lib/trip_list.dart
+++ lib/trip_list.dart
@@ -5,22 +5,38 @@ import 'object_models.dart';
 import 'package:intl/intl.dart';
 import 'ModifyDialog.dart';
 import 'budget_tracker.dart';
+import 'firebase.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'packing_list_tracker.dart';
+
 
 
 class TripList extends StatelessWidget {
-  // final List<Trip> trips;
-  // TripList({required this.trips});
+
 
   @override
   Widget build(BuildContext context) {
     final tripProvider = context.watch<TripProvider>();
+
     final trips = tripProvider.trips;
+
+
+  /*
+    List<Packing_List> packingList = tripProvider.getPackingList(3);
+    print(packingList);
+   */
+
+    FirebaseService firebaseService = FirebaseService();
+
+
+
     return ListView.builder(
       itemCount: trips.length,
       itemBuilder: (BuildContext context, int index) {
         final trip = trips[index];
         final startDateFormatted = DateFormat('yyyy-MM-dd hh:mm a').format(trip.start);
         final endDateFormatted = DateFormat('yyyy-MM-dd hh:mm a').format(trip.end);
+
         return ListTile(
           title: Text(trip.destination),
           subtitle: Text('$startDateFormatted - $endDateFormatted'),
@@ -33,12 +49,6 @@ class TripList extends StatelessWidget {
                   },
                   icon: const Icon(Icons.flight)
               ),
-              IconButton(
-                onPressed: () {
-                  _showModifyDialog(context, trip);
-                },
-                icon: Icon(Icons.edit),
-              ),
 
 
               IconButton(
@@ -47,10 +57,25 @@ class TripList extends StatelessWidget {
                 },
                 icon: Icon(Icons.account_balance_wallet),
               ),
+
               IconButton(
-                onPressed: (){ tripProvider.removeTrip(index);},
+                onPressed: () {
+                  _showPackingList(context, index);
+                },
+                icon: Icon(Icons.list),
+              ),
+              IconButton(
+                onPressed: () {
+                  _showModifyDialog(context, trip);
+                },
+                icon: Icon(Icons.edit),
+              ),
+              IconButton(
+                onPressed: () {
+                  _showDeleteConfirmation(context, index);
+                },
                 icon: const Icon(Icons.delete_forever),
-              )
+              ),
             ],
           ),
         );
@@ -66,6 +91,7 @@ class TripList extends StatelessWidget {
         final endDateFormatted = DateFormat('yyyy-MM-dd hh:mm a').format(trip.end);
         final checkInFormatted = DateFormat('yyyy-MM-dd hh:mm a').format(trip.hotel.checkIn);
         final checkOutFormatted = DateFormat('yyyy-MM-dd hh:mm a').format(trip.hotel.checkOut);
+
         return AlertDialog(
           title: Text(trip.destination),
           content: SingleChildScrollView(
@@ -83,6 +109,10 @@ class TripList extends StatelessWidget {
                 Text(' - Check-in: $checkInFormatted'),
                 Text(' - Check-out: $checkOutFormatted'),
                 Text(' - Room Number: ${trip.hotel.roomNum}'),
+
+
+
+
               ],
             ),
           ),
@@ -117,6 +147,42 @@ class TripList extends StatelessWidget {
     );
   }
 
+  void _showDeleteConfirmation(BuildContext context, int tripIndex) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete Trip'),
+          content: Text('Are you sure you want to delete this trip?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final tripProvider = Provider.of<TripProvider>(context, listen: false);
+                tripProvider.removeTrip(tripIndex);
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showPackingList(BuildContext context, int tripIndex) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PackingListTracker(tripIndex: tripIndex),
+      ),
+    );
+  }
 
 
 
