e023b5a8bcd55689237c5a38e9d6e29847bb2627
Casey Nguyen <caseyjnguyen@gmail.com>
2023-06-03 20:42:26 -0700

    More maps widget features.

--- lib/widgets/maps_widget.dart
+++ lib/widgets/maps_widget.dart
@@ -1,6 +1,8 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
+const LatLng currentLocation = LatLng(47.6062, 122.3321);
+
 class MapPage extends StatefulWidget{
   const MapPage({Key? key}): super(key:key);
   @override
@@ -8,11 +10,30 @@ class MapPage extends StatefulWidget{
 }
 
 class _MapPageState extends State<MapPage>{
+  late GoogleMapController mapController;
+  Map<String, Marker> _markers = {};
+  addMarker(String id, LatLng location){
+    var marker = Marker(
+      markerId: MarkerId(id),
+      position: location,
+    );
+    _markers[id] = marker;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(
-          target: LatLng(0,0)))
+    return Scaffold(
+      body: GoogleMap(
+          initialCameraPosition: const CameraPosition(
+          target: currentLocation,
+          zoom: 14),
+        onMapCreated: (controller){
+          mapController = controller;
+          addMarker('test', currentLocation);
+        },
+        markers: _markers.values.toSet(),
+      )
     );
   }
 }
